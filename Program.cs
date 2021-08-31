9579917afd8ff3b26793d58895f3502b738bdcc2
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-08-31 15:24:03 -0300

    Adiciona solução do exercício ebolinha

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fraseDigitada, fraseComDislalia;
+            Console.Write("Digite uma frase:");
+            fraseDigitada = Console.ReadLine();
+
+            fraseComDislalia = fraseDigitada
+            .Replace("r","l")
+            .Replace("R", "L");
+
+            Console.WriteLine(fraseComDislalia);
+        }
+    }
+}
